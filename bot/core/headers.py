c4e529e383f7596d05ec55a027bae0b502952ffd
YarmolenkoD <yarmolenko.dany@gmail.com>
2024-10-02 14:30:17 +0300

    update headers

--- bot/core/headers.py
+++ bot/core/headers.py
@@ -1,15 +1,14 @@
 headers = {
     'Accept': 'application/json, text/plain, */*',
-    'Accept-Language': 'ru,en;q=0.9,en-GB;q=0.8,en-US;q=0.7',
-    'Accept-Encoding': 'gzip, deflate, br, zstd',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'en-GB,en;q=0.9',
     'Content-Type': 'application/json',
-    'Origin': 'https://image.notpx.app',
-    'Referer': 'https://image.notpx.app/',
+    'Connection': 'keep-alive',
+    'Host': 'notpx.app',
+    'Origin': 'https://app.notpx.app',
+    'Referer': 'https://app.notpx.app/',
     'Sec-Fetch-Dest': 'empty',
     'Sec-Fetch-Mode': 'cors',
     'Sec-Fetch-Site': 'same-site',
-    'Sec-Ch-Ua': '"Chromium";v="128", "Not;A=Brand";v="24", "Microsoft Edge";v="128", "Microsoft Edge WebView2";v="128"',
-    'Sec-Ch-Ua-mobile': '?0',
-    'Sec-Ch-Ua-platform': '"Android"',
     'User-Agent': 'Mozilla/5.0 (Linux; Android 14) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.6422.165 Mobile Safari/537.36'
 }
