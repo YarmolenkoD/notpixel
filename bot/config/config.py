1d3881cb07379893f5a604cbaac1444f50beb84c
YarmolenkoD <yarmolenko.dany@gmail.com>
2024-10-15 21:50:25 +0300

    update default config values

--- bot/config/config.py
+++ bot/config/config.py
@@ -29,7 +29,7 @@ class Settings(BaseSettings):
     NIGHT_TIME: list[int] = [23, 6]
 
     ENABLE_SOCKETS: bool = False
-    ENABLE_RANDOM_CUSTOM_TEMPLATE: bool = True
+    ENABLE_RANDOM_CUSTOM_TEMPLATE: bool = False
     ENABLE_DRAW_CUSTOM_TEMPLATE: bool = True
     CUSTOM_TEMPLATE_ID: int = 355876562
 
